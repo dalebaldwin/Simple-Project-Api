d222d3b9db20b320456618b76c31da7fb74bb5b2
Dale Baldwin <dale.baldwin@rxpservices.com>
2017-02-17 10:57:57 +1100

    Separated config out into it's own json file.

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 ﻿const express = require('express')
 const bodyParser = require('body-parser')
 const expressMongoDb = require('express-mongo-db')
-
+const config = require('./config')
 const app = express()
 
-// Database name 
-
-const database = 'databaseName'
-
-// API Name and Basic Port Config
-
-const apiName = 'MyapiName'
-const apiVersion = "1.0.0"
-const apiPort = 3000
-
-app.use(expressMongoDb("mongodb://localhost:27017/" + database))
+app.use(expressMongoDb("mongodb://localhost:27017/" + config.database))
 app.use(require('body-parser').urlencoded({ extended: true }))
 app.use(require('body-parser').json())
 
 // Root Route that returns nothing
 
 app.get('/', function (req, res) {
-    res.send(apiName + " version " + apiVersion)
+    res.send(config.apiName + " version " + config.apiVersion)
 })
 
 // Custom Endpoints
 
 app.use('/v1/', require('./endpoints/v1/exampleEndpoint.js'))
 
-app.listen(apiPort, function () {
-    console.log(apiName + " version " + apiVersion + " is up and running on port " + apiPort)
+app.listen(config.apiPort, function () {
+    console.log(config.apiName + " version " + config.apiVersion + " is up and running on port " + config.apiPort)
 })
